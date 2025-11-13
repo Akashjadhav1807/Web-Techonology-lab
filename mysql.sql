30cb26d144ffec2ee397564eabfcf4f2907c1c79
Akashjadhav1807 <akash.23cs007@sode-edu.in>
2025-11-13 12:46:07 +0530

    Add files via upload

---
+++ mysql.sql
@@ -0,0 +1,5 @@
+show databases;
+create database student123;
+use students124;
+create table student123(id int primary key,name varchar(50),grade float);
+insert into student123 values(1,"v abc",99);
